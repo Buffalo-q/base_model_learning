98868741a65b98ada21359214dd99411174cce63
Buffalo-q <edward.5186li@gmail.com>
2023-12-18 00:22:22 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author  : Buffalo-q
+# @Time    : 2023/12/12 10:57
+# @File    : test.py
+# @Software: PyCharm
+
+
+from d2l import torch as d2l #
+
+print(d2l.matmul(d2l.ones(2, 3), d2l.ones(3, 2)))
+# print(d2l.zeros(3, 4))
---
+++ 云服务器使用/vscode服务器使用.md
@@ -0,0 +1,7 @@
+# AutoDL服务器的使用
+连接服务器、输入密码、使用命令行运行代码
+![Alt text](image-1.png)
+
+![Alt text](image.png)
+
+![Alt text](image-2.png)
